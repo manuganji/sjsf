e1de30e7e843b08c5164702401cbb35dbe426a71
Manu Ganji <manuganji@gmail.com>
2022-07-25 23:40:04 +0200

    Adding first test

---
+++ src/routes/examples/__layout.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import {} from 'svelte';
+</script>
+
+<div>
+	<ul><li><a href="/examples/integer">Integer</a></li></ul>
+	<slot />
+</div>
---
+++ src/routes/examples/integer.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import Form from '$lib/index.svelte';
+	import type { JSONSchema7 } from 'json-schema';
+	const schema: JSONSchema7 = {
+		type: 'object',
+		title: 'Integer',
+		properties: {
+			integer: {
+				type: 'integer',
+				title: 'Integer',
+				default: 0
+			}
+		}
+	};
+</script>
+
+<div>{JSON.stringify(schema)}</div>
+<Form {schema} />
