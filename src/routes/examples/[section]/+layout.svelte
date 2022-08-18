bb52bd1f1c3747e1983e22026718a038d74cb16f
Manu Ganji <manuganji@gmail.com>
2022-08-18 13:23:17 +0200

    Fixing reactive rendering of examples

--- src/routes/examples/[section]/+layout.svelte
+++ src/routes/examples/[section]/+layout.svelte
@@ -1,40 +1,49 @@
 <script lang="ts" context="module">
-  import { page } from '$app/stores';
   import type { LayoutData } from './$types';
+  import { goto } from '$app/navigation';
 </script>
 
 <script lang="ts">
+  import { page } from '$app/stores';
+
   export let data: LayoutData;
   const { keys } = data;
-  const sectionName = $page.params.section;
-  const title = sectionName[0].toUpperCase() + sectionName.slice(1);
+  let title: string, sectionName: string;
+  $: {
+    sectionName = $page.params.section;
+    title = sectionName[0].toUpperCase() + sectionName.slice(1);
+  }
 </script>
 
 <svelte:head>
-  <title>{title} Examples</title>
+  {#key title} <title>{title} Examples</title> {/key}
 </svelte:head>
 
-<div class="dark:bg-dark-7 h-screen w-full dark:text-white">
-  <ul class="flex flex-wrap gap-1 py-4 px-4 bg-gray-1 dark:bg-dark-5">
-    {#each keys as section}
-      <li
-        class="list-none
+{#key title}
+  <div class="dark:bg-dark-7 h-screen w-full dark:text-white">
+    <ul class="flex flex-wrap gap-1 py-4 px-4 bg-gray-1 dark:bg-dark-5">
+      {#each keys as section}
+        <li
+          on:click={() => {
+            goto(`/examples/${section}`);
+          }}
+          class="list-none
           bg-amber-1 dark:bg-amber-7
         border-amber border-2 
           border-solid p-2
           rounded-2
         hover:border-amber-5
           cursor-pointer"
-      >
-        <a
-          class="text-gray-6 hover:text-gray-7 dark:text-white no-underline"
-          href={`/examples/${section}`}>{section[0].toUpperCase() + section.slice(1)}</a
         >
-      </li>
-    {/each}
-  </ul>
-  {#key $page.params.section}
+          <a
+            class="text-gray-6 hover:text-gray-7 dark:text-white no-underline"
+            href={`/examples/${section}`}>{section[0].toUpperCase() + section.slice(1)}</a
+          >
+        </li>
+      {/each}
+    </ul>
+
     <h1 class="px-4">{title} Examples</h1>
     <slot />
-  {/key}
-</div>
+  </div>
+{/key}
--- src/routes/examples/[section]/+page.server.ts
+++ src/routes/examples/[section]/+page.server.ts
@@ -1,13 +1,13 @@
 import { redirect } from '@sveltejs/kit';
 import sections from './_sections';
-import type { PageServerLoad } from './$types';
+import type { PageServerLoad } from '../../../../.svelte-kit/types/src/routes/examples/[section]/$types';
 
 export const load: PageServerLoad = async ({ params }) => {
   if (!Object.hasOwn(sections, params.section)) {
     throw redirect(303, `/examples/string`);
   } else {
     return {
-      examples: sections[params.section],
-    }
+      examples: sections[params.section]
+    };
   }
 };
